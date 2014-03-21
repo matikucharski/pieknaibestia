d5c1c1427bc707a2acd8e0d3c6577544c1b5ce9e
matikucharski <mati.kucharski@gmail.com>
2014-03-21 01:34:32 +0100

    responsive menu fix

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,8 +1,23 @@
 Meteor.Router.add({
-	'/' : 'onas',
-	'/slub' : 'slub',
-	'/wesele' : 'wesele',
-	'/onas' : 'onas',
-	'/info' : 'info',
-	'/prezenty' : 'prezenty'
+	'/' : 'slub',
+	'/slub' : {
+		to:'slub',
+		and: function(){Session.set('activeMenu', 'slub');}
+	},
+	'/wesele' : {
+		to:'wesele',
+		and: function(){Session.set('activeMenu', 'wesele');}
+	},
+	'/onas' : {
+		to:'onas',
+		and: function(){Session.set('activeMenu', 'onas');}
+	},
+	'/info' : {
+		to:'info',
+		and: function(){Session.set('activeMenu', 'info');}
+	},
+	'/prezenty' : {
+		to:'prezenty',
+		and: function(){Session.set('activeMenu', 'prezenty');}
+	}
 });
--- client/jquery/jq.js
+++ client/jquery/jq.js
@@ -1,10 +1,15 @@
 $(function(){
+	//TODO change it to do it on reload - without click
+	var menuItem = Session.get('activeMenu');
+	$("ul.nav li#"+menuItem).addClass('active');
 
 	//highlights clicked menu item
 	$("ul.nav li").click(function(){
 		console.log(this);
 		$("li.active").removeClass('active');
 		$(this).addClass('active');
+		//hides menu after click
+		$("div.nav-collapse.collapse").collapse('hide');
 	});
 	//removes highlight from menu item
 	$("a.brand").click(function(){
